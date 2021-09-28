9383b32fa68d768d5b3d2c235a60f20ef1364b57
Sandra Milena Ortegon <x56-ella@hotmial.com>
2021-09-28 11:44:09 -0500

    Actualizacion con Repositorios para persona

--- Epets.App/Epets.App.Persistencia/AppData/MfAppContext.cs
+++ Epets.App/Epets.App.Persistencia/AppData/MfAppContext.cs
@@ -6,11 +6,11 @@ namespace Epets.App.Persistencia.AppData
 {
     public class MfAppContext:DbContext
     {
-        public DbSet<Epets.App.Dominio.Entidades.Persona> Personas{get;set;} //propiedad
-        public DbSet<Epets.App.Dominio.Entidades.Medico> Medicos{get;set;} //propiedad
-        public DbSet<Epets.App.Dominio.Entidades.Propietario> Propietarios{get;set;} //propiedad
-        public DbSet<Epets.App.Dominio.Entidades.Mascota> Mascotas{get;set;} //propiedad
-        public DbSet<Epets.App.Dominio.Entidades.Empresa> Empresa{get;set;} //propiedad
+        public DbSet<Persona> Personas{get;set;} //propiedad
+        public DbSet<Medico> Medicos{get;set;} //propiedad
+        public DbSet<Propietario> Propietarios{get;set;} //propiedad
+        public DbSet<Mascota> Mascotas{get;set;} //propiedad
+        public DbSet<Empresa> Empresa{get;set;} //propiedad
 
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
