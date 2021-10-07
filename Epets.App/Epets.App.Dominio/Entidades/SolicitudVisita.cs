49e9b3fa41508f244c7add706cfc33811ad34475
ElkinRamos <90712468+ElkinRamos@users.noreply.github.com>
2021-10-06 19:31:23 -0500

    Update SolicitudVisita.cs

--- Epets.App/Epets.App.Dominio/Entidades/SolicitudVisita.cs
+++ Epets.App/Epets.App.Dominio/Entidades/SolicitudVisita.cs
@@ -1,50 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Epets.App.Dominio.Entidades
-
 {
-     /// <summary>Class <c>SolicitudVisita</c>
+    /// <summary>Class <c>SolicitudVisita</c>
     /// Modela la solicitud que realiza el Propietario de la visita
     /// para la mascota
     /// </summary>   
   
- //   [Table("SolicitudVisitaDb")]
- //   1 reference
+    [Table("SolicitudVisitaDb")]
     public class SolicitudVisita
     {
-  //      [Column("Id")]
-  //      [Key]
-  //      0 references
+        [Column("Id")]
+        [Key]
         public int Id { get; set; }
         
-  //      [ForeignKey("Mascota")]
-  //      0 references
+        [ForeignKey("Mascota")]
         // Identificacion de la mascota
         public Mascota Mascota{get;set;}
         
-    //    [ForeignKey("FechaVisita")]
-    //    0 references
+        [ForeignKey("FechaVisita")]
         /// Fecha y hora en que se agenda la visita       
         public DateTime FechaVisita{get;set;}
         
-    //    [ForeignKey("Propietario")]
-    //    0 references
+        [ForeignKey("Propietario")]
         /// Relacion entre el propietario y la solicitud de Visita
         public Propietario Propietario{get;set;}
         
-    //    [ForeignKey("NombreMascota")]
-    //    0 references
+        [ForeignKey("NombreMascota")]
         /// Relacion entre la Mascota y la solicitud de Visita
         public Mascota NombreMascota{get;set;}
         
-    //    [ForeignKey("TipoAnimal")]
-    //    0 references
+/*        [ForeignKey("TipoAnimal")]
         /// Relacion entre el Tipo de Animal y la solicitud de Visita
-        public TipoAnimal TipoAnimal{get;set;}
+        public TipoAnimal TipoAnimal{get;set;}*/
         
-    //    [ForeignKey("Especialidad")]
-    //    0 references
+/*        [ForeignKey("Especialidad")]
         /// Relacion entre la Especialidad del Medico y la solicitud de Visita
-        public Especialidad Especialidad{get;set;}
+        public Especialidad Especialidad{get;set;}*/
     }
 }
