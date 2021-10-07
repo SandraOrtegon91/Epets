c17884b8bfd05b668215622f502b49f2339ea486
ElkinRamos <90712468+ElkinRamos@users.noreply.github.com>
2021-10-06 19:38:30 -0500

    Update RepositorioMascota.cs

--- Epets.App/Epets.App.Persistencia/AppRepositorios/RepositorioMascota.cs
+++ Epets.App/Epets.App.Persistencia/AppRepositorios/RepositorioMascota.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Linq;
 using System.Collections.Generic;
 using Epets.App.Dominio.Entidades;
-using Epets.App.Persistencia.AppData;
+//using Epets.App.Persistencia.AppData;
 
 namespace Epets.App.Persistencia.AppRepositorios
 {
@@ -11,14 +10,14 @@ namespace Epets.App.Persistencia.AppRepositorios
         /// <summary>
         /// Referencia al contexto de la Mascota
         /// </summary>
-        private readonly MfAppContext _appContext;
+        private readonly Epets.App.Persistencia.AppData.MfAppContext _appContext;
 
         /// <summary>Metodo constructor
         /// que utiliza la inyeccion de dependencias
         /// para indicar el contexto a utilizar </summary>
         /// <param name="appContext"></param>
   
-        public RepositorioMascota(MfAppContext appContext)
+        public RepositorioMascota(Epets.App.Persistencia.AppData.MfAppContext appContext)
         {
             _appContext=appContext;
         } 
@@ -27,7 +26,7 @@ namespace Epets.App.Persistencia.AppRepositorios
         //Implementacion metodo Añadir Mascota
         Mascota IRepositorioMascota.AddMascota(Mascota mascota)
         {
-            var mascotaAdicionada=_appContext.mascotas.Add(mascota);
+            var mascotaAdicionada=_appContext.Mascotas.Add(mascota);
             _appContext.SaveChanges();
             return mascotaAdicionada.Entity;
         }
@@ -35,12 +34,12 @@ namespace Epets.App.Persistencia.AppRepositorios
         //Implementacion metodo Eliminar una Mascota 
         void IRepositorioMascota.DeleteMascota(int IdMascota)
         {
-            var mascotaEncontrada=_appContext.mascotas.FirstOrDefault(p => p.Id==IdMascota);
+            var mascotaEncontrada=_appContext.Mascotas.FirstOrDefault(p => p.IdMascota==IdMascota);
 
             if (mascotaEncontrada==null)
                 return ;
             
-            _appContext.mascotas.Remove(mascotaEncontrada);
+            _appContext.Mascotas.Remove(mascotaEncontrada);
             _appContext.SaveChanges();
 
         }
@@ -48,22 +47,21 @@ namespace Epets.App.Persistencia.AppRepositorios
         //Implementacion metodo retornar todas las Mascotas
         IEnumerable<Mascota> IRepositorioMascota.GetAllMascotas()
         {
-            return _appContext.mascotas;
+            return _appContext.Mascotas;
 
         }
 
         //Implementacion metodo Retornar una Mascota
         Mascota IRepositorioMascota.GetMascota(int IdMascota)
         {
-            return _appContext.mascotas.FirstOrDefault(p => p.Id==IdMascota);
+            return _appContext.Mascotas.FirstOrDefault(p => p.IdMascota==IdMascota);
 
         }
 
         //Implementacion metodo Actualizar Mascota
         Mascota IRepositorioMascota.UpdateMascota(Mascota mascota)
         {
-
-            var mascotaEncontrada=_appContext.mascotas.FirstOrDefault(p => p.Id==mascota.Id);
+            var mascotaEncontrada=_appContext.Mascotas.FirstOrDefault(p => p.IdMascota==mascota.IdMascota);
 
             if (mascotaEncontrada!=null)
                 {
@@ -73,13 +71,10 @@ namespace Epets.App.Persistencia.AppRepositorios
                     mascotaEncontrada.Raza=mascota.Raza;
                     mascotaEncontrada.Propietario=mascota.Propietario;
                     mascotaEncontrada.Medico=mascota.Medico;
-                    mascotaEncontrada.TipoAnimal=mascota.TipoAnimal;
+//                    mascotaEncontrada.TipoAnimal=mascota.TipoAnimal;
                     mascotaEncontrada.Historia=mascota.Historia;
-
-                    _appContext.SaveChanges();
-                   
-                }
-            
+                    _appContext.SaveChanges();                   
+                }            
             return mascotaEncontrada;
         }
     }
