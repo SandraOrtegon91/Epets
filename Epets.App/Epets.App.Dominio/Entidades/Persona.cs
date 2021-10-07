a2f3960f9e4e0e06ec1316239cceba46856e267a
ElkinRamos <90712468+ElkinRamos@users.noreply.github.com>
2021-10-06 19:28:16 -0500

    Update Persona.cs

--- Epets.App/Epets.App.Dominio/Entidades/Persona.cs
+++ Epets.App/Epets.App.Dominio/Entidades/Persona.cs
@@ -1,46 +1,43 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Epets.App.Dominio.Entidades
 {
-     /// <summary>Class <c>Persona</c>
+    /// <summary>Class <c>Persona</c>
     /// Modela a la Persona con los atributos que comparten
     ///  en este caso son un Medico o un Propietario
     /// </summary>   
   
-   //[Table("PersonaDb")]
-   // 1 reference
+    [Table("PersonaDb")]
     public class Persona
     {
-     //   [Column("Id")]
-     //   [Key]
-     //   0 references
+        [Column("Id")]
+        [Key]
         // Identificador único de cada Persona
         public int Id{get;set;}
         
-     //   [Required]
-     //   [Column("Cedula")]
-     //   [StringLength(10,MinimumLength=5)]
-     //   0 references
+        [Required]
+        [Column("Cedula")]
+        [StringLength(10,MinimumLength=5)]
         //Cedula de la persona
         public string Cedula{get; set;}
         
-     //   [Required]
-     //   [Column("Nombre")]
-     //   [StringLength(10,MinimumLength=5)]
-     //   0 references
+        [Required]
+        [Column("Nombre")]
+        [StringLength(10,MinimumLength=5)]
         //Nombre de la Persona
         public string Nombre{get;set;}
         
-    //    [Required]
-    //    [Column("Apellido")]
-    //    [StringLength(10,MinimumLength=5)]
-    //    0 references
+        [Required]
+        [Column("Apellido")]
+        [StringLength(10,MinimumLength=5)]
         //Apellido de la Mascota
         public string Apellido{get;set;}
         
-    //    [Required]
-    //    [Column("Telefono")]
-    //    [StringLength(10,MinimumLength=5)]
-    //    0 references
+        [Required]
+        [Column("Telefono")]
+        [StringLength(10,MinimumLength=5)]
         //Telefono de la Persona
         public string Telefono{get;set;}
     }
