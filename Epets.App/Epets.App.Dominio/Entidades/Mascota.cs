d1fbe377f4a60a1dc6c19748cc0176e987b8daaf
ElkinRamos <90712468+ElkinRamos@users.noreply.github.com>
2021-10-06 19:23:25 -0500

    Update Mascota.cs

--- Epets.App/Epets.App.Dominio/Entidades/Mascota.cs
+++ Epets.App/Epets.App.Dominio/Entidades/Mascota.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Epets.App.Dominio.Entidades
 {
@@ -8,66 +10,55 @@ namespace Epets.App.Dominio.Entidades
     /// </summary>   
 
   //  [Table("MascotaDb")]
-  //  1 reference
     public class Mascota
     {
   //      [Column("Id")]
   //      [Key]
-  //      0 references
         // Identificador único de cada Mascota
         public int Id{get;set;}
         
    //     [Required]
    //     [Column("NombreMascota")]
    //     [StringLength(10,MinimumLength=5)]
-   //     0 references
         //Nombre de La Mascota
         public string NombreMascota{get;set;}
         
     //    [Required]
     //    [Column("ColorOjos")]
     //    [StringLength(10,MinimumLength=5)]
-    //    0 references
         //Color de Ojos de la Mascota
         public string ColorOjos{get;set;}
         
     //    [Required]
     //    [Column("ColorPiel")]
     //    [StringLength(10,MinimumLength=5)]
-    //    0 references
         //Color del pelaje o plumaje de la Mascota
         public string ColorPiel{get;set;}
         
     //    [Required]
     //    [Column("Estatura")]
-    //    0 references
         //Estatura de la Mascota
         public float Estatura{get;set;}
         
     //    [Required]
     //    [Column("Raza")]
     //    [StringLength(10,MinimumLength=5)]
-    //    0 references
         //Raza de la mascota
         public string Raza{get;set;}
         
     //    [ForeignKey("Propietario")]
-    //    0 references
         /// Relacion entre el propietario y la Mascota
         public Propietario Propietario{get;set;}
         
     //    [ForeignKey("Medico")]
-    //    0 references
         /// Relacion entre el Medico y la Mascota
         public Medico Medico{get;set;}
         
     //    [ForeignKey("TipoAnimal")]
-    //    0 references
         /// Relacion entre el Tipo de Animal y la Mascota
         public TipoAnimal TipoAnimal{get;set;}
         
     //    [ForeignKey("Historia")]
-    //    0 references
         /// Relacion entre Mascota y su Historia clínica
         public Historia Historia{get;set;}
     }
