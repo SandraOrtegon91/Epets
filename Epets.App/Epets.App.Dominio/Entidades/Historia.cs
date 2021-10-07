ab888cf2335f26e4af76be48d63452d7b0ee7d64
ElkinRamos <90712468+ElkinRamos@users.noreply.github.com>
2021-10-06 19:21:09 -0500

    Update Historia.cs

--- Epets.App/Epets.App.Dominio/Entidades/Historia.cs
+++ Epets.App/Epets.App.Dominio/Entidades/Historia.cs
@@ -1,45 +1,40 @@
 using System;
 using System.Collections.Generic;
 namespace Epets.App.Dominio.Entidades
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 {
    /// <summary>Class <c>Historia</c>
     /// Modela la Historia clínica relacionada con la atencion de la Mascota
     /// </summary>    
     
   //  [Table("Historia")]
-  //  1 reference
     public class Historia
     {
     //    [Column("Id")]
     //    [Key]
-    //    0 references
         // Identificador único de la Historia
         public int Id { get; set; }
         
     //    [Required]
     //    [Column("DateTime")]
     //    [StringLength(12,MinimumLength=6)]
-    //    0 references
         public DateTime FechaVisita{get;set;} //DateTime
         
     //    [Required]
     //    [Column("Recomendaciones")]
     //    [StringLength(50,MinimumLength=5)]
-    //    0 references
      //   public string Recomendaciones{get;set;}
         
     //    [Required]
     //    [Column("Medicamento")]
     //    [StringLength(20,MinimumLength=5)]
-    //    0 references
         public string Medicamento{get;set;}
         
     //    [ForeignKey("SignoVital")]
-    //    0 references
         public SignoVital Signos{get;set;}
         
     //    [ForeignKey("SignoVitale")]
-    //    0 references
         //Referencia a la lista de signos vitales de una mascota
         public System.Collections.Generic.List<SignoVital> SignosVitales { get; set; }
         
